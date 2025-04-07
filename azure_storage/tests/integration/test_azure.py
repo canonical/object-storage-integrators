908e45ae239f7fb7cc2c4183d2415b10fb766411
Alex Batisse <alex.batisse@canonical.com>
2025-04-07 15:10:34 +0200

    refactor: Do not build charms in integration test

--- azure_storage/tests/integration/test_azure.py
+++ azure_storage/tests/integration/test_azure.py
@@ -36,14 +36,30 @@ FIRST_RELATION = "first-azure-storage-credentials"
 SECOND_RELATION = "second-azure-storage-credentials"
 
 
+@pytest.fixture
+def azure_charm() -> Path:
+    if not (path := next(iter(Path.cwd().glob("*.charm")), None)):
+        raise FileNotFoundError("Could not find packed azure-storage-integrator charm.")
+
+    return path
+
+
+@pytest.fixture
+def test_charm() -> Path:
+    if not (
+        path := next(
+            iter((Path.cwd() / "tests/integration/test-charm-azure").glob("*.charm")), None
+        )
+    ):
+        raise FileNotFoundError("Could not find packed test charm.")
+
+    return path
+
+
 @pytest.mark.abort_on_fail
 @pytest.mark.skip_if_deployed
-async def test_build_and_deploy(ops_test: OpsTest):
+async def test_build_and_deploy(ops_test: OpsTest, azure_charm: Path, test_charm: Path) -> None:
     """Build the charm and deploy 1 units for provider and requirer charm."""
-    # Build and deploy charm from local source folder
-    azure_charm = await ops_test.build_charm(".")
-    test_charm = await ops_test.build_charm("./tests/integration/test-charm-azure/")
-
     await asyncio.gather(
         ops_test.model.deploy(azure_charm, application_name=CHARM_NAME, num_units=1),
         ops_test.model.deploy(test_charm, application_name=TEST_APP_NAME, num_units=1),
