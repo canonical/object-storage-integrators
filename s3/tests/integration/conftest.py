10d9e2829e742d1f0f958feed4c7caf4b97ea161
Alex Batisse <alex.batisse@canonical.com>
2025-05-09 10:27:25 +0200

    chore: Bump jubilant to stable version

--- s3/tests/integration/conftest.py
+++ s3/tests/integration/conftest.py
@@ -34,9 +34,26 @@ def test_charm() -> Path:
 
 
 @pytest.fixture(scope="module")
-def juju():
-    with jubilant.temp_model() as juju:
-        yield juju
+def juju(request: pytest.FixtureRequest):
+    keep_models = bool(request.config.getoption("--keep-models"))
+
+    with jubilant.temp_model(keep=keep_models) as juju:
+        juju.wait_timeout = 10 * 60
+
+        yield juju  # run the test
+
+        if request.session.testsfailed:
+            log = juju.debug_log(limit=30)
+            print(log, end="")
+
+
+def pytest_addoption(parser):
+    parser.addoption(
+        "--keep-models",
+        action="store_true",
+        default=False,
+        help="keep temporarily-created models",
+    )
 
 
 @pytest.fixture(scope="module")
