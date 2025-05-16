b8ef510224b499ef0bb7a516049a1d7637c2b756
Alex Batisse <alex.batisse@canonical.com>
2025-05-16 11:49:06 +0200

    fix: Do not write None value to relation databag

--- s3/src/managers/s3.py
+++ s3/src/managers/s3.py
@@ -19,8 +19,11 @@ class S3Manager(WithLogging):
         """Update the contents of the relation data bag."""
         if len(self.relation_data.relations) > 0 and s3_connection_info:
             for relation in self.relation_data.relations:
-                requested_bucket = self.relation_data.fetch_relation_field(
-                    relation_id=relation.id, field="bucket"
+                requested_bucket = (
+                    self.relation_data.fetch_relation_field(
+                        relation_id=relation.id, field="bucket"
+                    )
+                    or ""
                 )
                 self.relation_data.update_relation_data(
                     relation.id, {"bucket": requested_bucket} | dict(s3_connection_info)
