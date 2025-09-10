2aedc2b4dda7340bf46bf6a74738388c6bdf03cd
gatici <gulsum.atici@canonical.com>
2025-09-10 15:23:34 +0300

    feat: add GCS integrator charm
    Signed-off-by: gatici <gulsum.atici@canonical.com>

---
+++ gcs/src/constants.py
@@ -0,0 +1,12 @@
+# Copyright 2024 Canonical Ltd.
+# See LICENSE file for licensing details.
+
+"""File containing constants to be used in the charm."""
+
+GCS_RELATION_NAME = "gcs-credentials"
+
+GCS_MANDATORY_OPTIONS = ["bucket", "credentials"]
+
+KEYS_LIST = ["service-account-json-secret"]
+OPTIONAL_OVERRIDE = "bucket"
+CREDENTIAL_FIELD = "credentials"
