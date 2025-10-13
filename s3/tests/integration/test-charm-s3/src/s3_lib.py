55a258c76b3f3afc8a2766fdc6ef6e7beec68c4c
Bikalpa Dhakal <bikalpa.dhakal@canonical.com>
2025-10-13 12:56:38 +0545

    wip

--- s3/tests/integration/test-charm-s3/src/s3_lib.py
+++ s3/tests/integration/test-charm-s3/src/s3_lib.py
@@ -232,7 +232,7 @@ class S3RequirerEventHandlers(RequirerEventHandlers):
         event_data = {
             "bucket": self.relation_data.bucket,
             "path": self.relation_data.path,
-            "version": str(LIBAPI),
+            "version": f"{LIBAPI}.{LIBPATCH}",
         }
         self.relation_data.update_relation_data(event.relation.id, event_data)
 
@@ -253,6 +253,13 @@ class S3RequirerEventHandlers(RequirerEventHandlers):
         diff = self._diff(event)
         if any(newval for newval in diff.added if self.relation_data._is_secret_field(newval)):
             self.relation_data._register_secrets_to_relation(event.relation, diff.added)
+
+        provider_lib_version = float(self.relation_data.fetch_relation_field(event.relation.id, "version") or "0")
+        if provider_lib_version < 1 and not self.relation_data.fetch_my_relation_field(event.relation.id, "bucket"):
+            # The following line exists here due to compatibility for v1 requirer to work with v0 provider
+            # The v0 provider will still wait for `bucket` to appear in the databag, and if it does not exist,
+            # the provider will simply not write any data to the databag.
+            self.relation_data.update_relation_data(event.relation.id, {"bucket": f"relation-{event.relation.id}"})
 
         # check if the mandatory options are in the relation data
         contains_required_options = True
@@ -397,8 +404,10 @@ class S3ProviderData(ProviderData):
         data_from_requirer = super().fetch_relation_data(
             [relation.id], [REQ_SECRET_FIELDS, self.RESOURCE_FIELD], relation.name
         )
+        keys = set(data.keys())
         if (
-            data_from_requirer.get(relation_id, {}).get(REQ_SECRET_FIELDS) is None
+            keys - {"version"}
+            and data_from_requirer.get(relation_id, {}).get(REQ_SECRET_FIELDS) is None
             and data_from_requirer.get(relation_id, {}).get(self.RESOURCE_FIELD) is None
         ):
             raise PrematureDataAccessError(
@@ -415,6 +424,25 @@ class S3ProviderEventHandlers(EventHandlers):
     def __init__(self, charm: CharmBase, relation_data: S3ProviderData, unique_key: str = ""):
         super().__init__(charm, relation_data, unique_key)
         self.relation_data = relation_data
+        # self.framework.observe(
+        #     self.charm.on[self.relation_data.relation_name].relation_joined, self._on_relation_joined_event
+        # )
+
+    def _on_relation_created_event(self, event: RelationJoinedEvent) -> None:
+        """Event emitted when the S3 relation is created."""
+        logger.debug(f"S3 relation ({event.relation.name}) created on provider side...")
+        event_data = {
+            "version": f"{LIBAPI}.{LIBPATCH}",
+        }
+        self.relation_data.update_relation_data(event.relation.id, event_data)
+    
+    # def _on_relation_joined_event(self, event: RelationJoinedEvent) -> None:
+    #     """Event emitted when the S3 relation is joined."""
+    #     logger.debug(f"S3 relation ({event.relation.name}) joined on provider side...")
+    #     event_data = {
+    #         "version": f"{LIBAPI}.{LIBPATCH}",
+    #     }
+    #     self.relation_data.update_relation_data(event.relation.id, event_data)
 
     def _on_relation_changed_event(self, event: RelationChangedEvent):
         if not self.charm.unit.is_leader():
